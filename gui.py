b0ee6fe9555b0b075b575f368fe21ffc056472a4
alexistucker <ajatucker@outlook.com>
2023-01-10 22:51:45 -0500

    adding some button stuff
    clicky buttons, changing colors of buttons, adding date solver

--- gui.py
+++ gui.py
@@ -4,9 +4,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QToolButton, QMainWindow
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 import sys
-
-import sys
-
+from datetime import datetime
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QApplication,
@@ -28,8 +26,24 @@ from PyQt5.QtWidgets import (
     QSpinBox,
     QTimeEdit,
     QVBoxLayout,
-    QWidget,
+    QWidget
 )
+
+class CommitList():
+   def __init__(self, c_list):
+        self.c_list = []
+   def add_to_list(self, num):
+        self.c_list.append(num)
+
+class CommitSolver():
+    def solve_datetime(self, num):
+        num.rjust(3 + len(num), '0')
+        curr_year = "2023"
+        res = datetime.strptime(curr_year + "-" + num, "%Y-%j").strftime("%Y-%m-%d")
+        str_res = str(res) + " 20:00"
+        return str_res
+
+    #def make_commit(self):
 
 
 # Subclass QMainWindow to customize your application's main window
@@ -41,48 +55,49 @@ class MainWindow(QMainWindow):
             print(self.sender().styleSheet())
             style = self.sender().styleSheet()
 
-            if (style == "") :
+            if (style == "background-color : #e6eef1") :
                 self.sender().setStyleSheet("background-color : #9be9a8")
             elif (style == "background-color : #9be9a8"):
                 self.sender().setStyleSheet("background-color : #40c463")
             elif (style == "background-color : #40c463"):
                 self.sender().setStyleSheet("background-color : #30a14e")
             elif (style == "background-color : #30a14e"):
-                self.sender().setStyleSheet("background-color : #216e39")
+                self.sender().setStyleSheet("background-color : #216e39") 
             else:
-                self.sender().setStyleSheet("")
-            
+                self.sender().setStyleSheet("background-color : #e6eef1")
 
-
-
-
+        self.setStyleSheet("background-color : #333333")
         self.setWindowTitle("Contributions Pixel Art")
         self.setGeometry(100, 60, 1429, 400)
- 
-        # widgets = [
-        #     QComboBox,
-        #     QDateEdit,
-        #     QDateTimeEdit,
-        #     QLabel,
-        #     QPushButton,
-        #     QTimeEdit,
-        # ]
+
+        solve = CommitSolver()
+
         i = 1
         j = 1
         day_count = 0
         for i in range(53):
             for j in range(7):
                 day_count = day_count + 1
-                num = str(day_count)
-                newBtn = QPushButton(num, self)
+                newBtn = QPushButton("", self)
                 newBtn.resize(25, 25)
                 newBtn.move(i*27, j*27)
+                newBtn.setStyleSheet("background-color : #e6eef1")
+                newBtn.setToolTip(solve.solve_datetime(str(day_count)))
                 newBtn.show()
                 newBtn.clicked.connect(lambda state, x=day_count: button_click(newBtn))
                 if(day_count == 365):
                     break
             if(day_count == 365):
                     break
+        
+        commitBtn = QPushButton("Commit!", self)
+        commitBtn.setStyleSheet("background-color : #4183C4")
+        commitBtn.resize(200, 100)
+        commitBtn.move(475, 250)
+        clearBtn = QPushButton("Clear", self)
+        clearBtn.setStyleSheet("background-color : #BD2C00")
+        clearBtn.resize(200, 100)
+        clearBtn.move(750, 250)
 
         # for w in widgets:
         #     layout.addWidget(w())
